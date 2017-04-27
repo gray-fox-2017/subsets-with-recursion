a2666f9242f7492141d316fb58af58cccd007ba1
ppsari <poppymighty@gmail.com>
2017-04-27 15:30:44 +0700

    p1w1d3_2_subsetrecursive

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,7 +1,9 @@
 let choose_team = (n, k) => {
-  //write your code here
+  if (k >= 1 && n - 1 >= k) {
+    return parseInt(choose_team(n-1,k-1)) + parseInt(choose_team(n-1,k)) ;
+  }else return 1;
 }
 
-console.log(choose_team(6, 2));
-console.log(choose_team(6, 3));
-console.log(choose_team(24, 4));
+console.log(choose_team(6, 2)); //15
+console.log(choose_team(6, 3)); //20
+console.log(choose_team(24, 4)); //10626
