70279eaee8433d0c468630801854c246bf799591
ingelieur <ingelieur@gmail.com>
2017-04-27 16:59:31 +0700

    lieur.out

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,13 +1,13 @@
 let choose_team = (n, k) => {
-  if (k===1) {return n;}
-  else {
-    comb1 = choose_team(n-1,k-1);
-    
+  if ((n-1) >= k && k >= 1) {
     return choose_team(n-1,k-1) + choose_team(n-1,k);
+  }
+  else {
+    return 1;
   }
 }
 console.log(choose_team(6, 1));
-console.log(choose_team(6, 2));/*
+console.log(choose_team(6, 2));
 console.log(choose_team(6, 3));
 console.log(choose_team(24, 4));
-*/
+
