63d9bf5110f753c6cde1836aa79404e88da993b0
ingelieur <ingelieur@gmail.com>
2017-04-27 16:46:23 +0700

    going through the hard way

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,7 +1,13 @@
 let choose_team = (n, k) => {
-  //write your code here
+  if (k===1) {return n;}
+  else {
+    comb1 = choose_team(n-1,k-1);
+    
+    return choose_team(n-1,k-1) + choose_team(n-1,k);
+  }
 }
-
-console.log(choose_team(6, 2));
+console.log(choose_team(6, 1));
+console.log(choose_team(6, 2));/*
 console.log(choose_team(6, 3));
 console.log(choose_team(24, 4));
+*/
