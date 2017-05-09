446d36f5eb5396be6cca86d0730f4465fc17cc0e
Sidik Hidayatullah <sidik.riders@gmail.com>
2017-05-09 14:16:35 +0700

    selesai di check

--- recursive_methods.js
+++ recursive_methods.js
@@ -2,15 +2,13 @@ let choose_team = (n, k) => {
   //write your code here
   var n2 = n-1;
   var k2 = k-1;
-  var n3 = n-1;
-  var k3 = k;
 
   if (k === 1) {
     return n;
   } else if (k === n){
     return 1
   } else {
-    return choose_team(n2,k2) + choose_team(n3, k3);
+    return choose_team(n2,k2) + choose_team(n2, k);
   }
 }
 
@@ -19,3 +17,4 @@ console.log(choose_team(6, 6));
 console.log(choose_team(24, 4));
 console.log(choose_team(6, 2));
 console.log(choose_team(6, 3));
+console.log(choose_team(4, 2));
