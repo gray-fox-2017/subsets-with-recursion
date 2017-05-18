8fd83fcb14f069f8c1602072e14edaa24d7dc907
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-05-19 00:27:49 +0700

    done

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,7 +1,11 @@
 let choose_team = (n, k) => {
   //write your code here
+  if ((1 <= k) && (k <= n-1)) {
+    return choose_team(n-1, k-1) + choose_team(n-1, k)
+  }
+  return 1
 }
 
-console.log(choose_team(6, 2));
-console.log(choose_team(6, 3));
-console.log(choose_team(24, 4));
+console.log(choose_team(6, 2)); //15
+console.log(choose_team(6, 3)); //20
+console.log(choose_team(24, 4)); //10626
